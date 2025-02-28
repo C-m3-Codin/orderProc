7245fa2339bef6a9a6507ef61faea4d9f51dce38
c1337 <cyril199897@gmail.com>
2025-03-01 02:06:54 +0530

    update interface

--- repository/Interfaces.go
+++ repository/Interfaces.go
@@ -1,5 +1,8 @@
 package repository
 
+import "gorm.io/gorm"
+
 type OrderRepoInteface interface {
-	GetOrder(orderid string)
+	GetOrder(orderid string) (order gorm.Model, err error)
+	CreateOrder(order gorm.Model) (err error)
 }
