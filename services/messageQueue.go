1c818f2ce8b13e83dbf86724836bd1458f80f696
c1337 <cyril199897@gmail.com>
2025-03-01 03:40:35 +0530

    start mq

---
+++ services/messageQueue.go
@@ -0,0 +1,20 @@
+package services
+
+import "c-m3-codin/ordProc/models"
+
+type Queue struct {
+	PendingQueue chan models.Order
+	Processing   chan models.Order
+}
+
+func NewQueue() (q Queue) {
+	pend := make(chan models.Order, 2000)
+	proc := make(chan models.Order, 2000)
+
+	q = Queue{
+		PendingQueue: pend,
+		Processing:   proc,
+	}
+	return q
+
+}
